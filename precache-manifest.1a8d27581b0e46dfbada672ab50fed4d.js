c0b9e953f19f9f05cf46626dc1686e9a1a5f9a1a
CodeCrypt <74805584+codecrypt112@users.noreply.github.com>
2022-05-06 07:23:56 +0530

    Add files via upload

---
+++ precache-manifest.1a8d27581b0e46dfbada672ab50fed4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd4db636e6147d64327f",
+    "url": "/static/css/main.cc1074a5.chunk.css"
+  },
+  {
+    "revision": "dd4db636e6147d64327f",
+    "url": "/static/js/main.dd4db636.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "3252effa48aa5e61f839",
+    "url": "/static/js/2.3252effa.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e7efa6e24cce2b5c679ac20748dafb92",
+    "url": "/index.html"
+  }
+];
